7481beb0968810ad21eed2087b1e134877276836
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-26 07:18:08 +0100

    Implemented pokeball-spinner for pokemon-list section

--- src/app/poke-ui/poke-ui.module.ts
+++ src/app/poke-ui/poke-ui.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 import { PokemonsComponent } from './pokemons/pokemons.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { InputComponent } from './components/input/input.component';
 import { ArrowComponent } from './components/arrow/arrow.component';
 import { RadarChartComponent } from './components/radar-chart/radar-chart.component';
-import { ChartsModule } from 'ng2-charts';
+import { PokeballSpinnerComponent } from './components/pokeball-spinner/pokeball-spinner.component';
 
 const ROUTES: Routes = [
   {
@@ -29,6 +30,7 @@ const ROUTES: Routes = [
     InputComponent,
     ArrowComponent,
     RadarChartComponent,
+    PokeballSpinnerComponent,
   ],
   imports: [
     CommonModule,
