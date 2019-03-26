bff5baeec7922346b825798e6056756c6e4d24d6
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-26 10:47:24 +0100

    Handled error fetch pokemon details by redirect to root page

--- src/app/poke-ui/pokemon-details/pokemon-details.service.ts
+++ src/app/poke-ui/pokemon-details/pokemon-details.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { PokemonDetails } from './pokemon-details.model';
 import { PokemonDetailsApiService } from '../services/pokemon-details-api.service';
 
@@ -10,12 +11,15 @@ import { PokemonDetailsApiService } from '../services/pokemon-details-api.servic
 export class PokemonDetailsService {
   pokemonDetails$: Subject<PokemonDetails> = new Subject();
 
-  constructor(private pokemonDetailsApiService: PokemonDetailsApiService) {}
+  constructor(private router: Router, private pokemonDetailsApiService: PokemonDetailsApiService) {}
 
   fetchPokemonDetails(id: string): void {
     this.pokemonDetailsApiService
       .getPokemon(id)
-      .pipe(tap(pokemonDetails => this.pokemonDetails$.next(pokemonDetails)))
+      .pipe(
+        tap(pokemonDetails => this.pokemonDetails$.next(pokemonDetails)),
+        catchError(() => this.router.navigate(['./'])),
+      )
       .subscribe();
   }
 }
