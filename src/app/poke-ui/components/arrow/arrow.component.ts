75ab258ac78a846fa7ba578324c53e7532d42df3
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-26 09:03:25 +0100

    Refactored arrow.component

--- src/app/poke-ui/components/arrow/arrow.component.ts
+++ src/app/poke-ui/components/arrow/arrow.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-arrow',
   templateUrl: './arrow.component.html',
   styleUrls: ['./arrow.component.scss'],
 })
-export class ArrowComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class ArrowComponent {}
