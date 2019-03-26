18c6e54b0ad4d7da667d74274dc8447d9afd559e
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-26 08:55:26 +0100

    Refactored container pokemons.component

--- src/app/poke-ui/pokemons/pokemons.component.ts
+++ src/app/poke-ui/pokemons/pokemons.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { PokemonsService } from './pokemons.service';
 import { Pokemon } from './pokemon.model';
-import { tap } from 'rxjs/operators';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 const POKEMON_COUNT = 807;
 
@@ -14,16 +14,16 @@ const POKEMON_COUNT = 807;
   styleUrls: ['./pokemons.component.scss'],
 })
 export class PokemonsComponent implements OnInit {
+  form: FormGroup;
+  offset = 0;
+  limit = 10;
   loading = true;
   pokemons$: Observable<Pokemon[]> = this.pokemonsService.pokemons$.pipe(
     tap(() => (this.loading = false)),
   );
 
-  form: FormGroup;
-  offset = 0;
-  limit = 10;
-
   private pokemonCount = POKEMON_COUNT;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -32,11 +32,7 @@ export class PokemonsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const limit = this.route.snapshot.queryParamMap.get('limit');
-    const offset = this.route.snapshot.queryParamMap.get('offset');
-    this.limit = limit ? Number(limit) : this.limit;
-    this.offset = offset ? Number(offset) : this.offset;
-    this.actualizeQueryParams();
+    this.setQueryParamsState();
     this.pokemonsService.fetchPokemons({ offset: this.offset, limit: this.limit });
     this.form = this.formBuilder.group({
       search: [null, [Validators.required, Validators.max(this.pokemonCount), Validators.min(1)]],
@@ -50,12 +46,20 @@ export class PokemonsComponent implements OnInit {
     this.pokemonsService.fetchPokemons({ offset: this.offset, limit: this.limit });
   }
 
-  private actualizeQueryParams() {
+  private actualizeQueryParams(): void {
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: { limit: this.limit, offset: this.offset },
       queryParamsHandling: 'merge',
       skipLocationChange: false,
     });
+  }
+
+  private setQueryParamsState(): void {
+    const limit = this.route.snapshot.queryParamMap.get('limit');
+    const offset = this.route.snapshot.queryParamMap.get('offset');
+    this.limit = limit ? Number(limit) : this.limit;
+    this.offset = offset ? Number(offset) : this.offset;
+    this.actualizeQueryParams();
   }
 }
