0ff991b38f98b18a785040e6d4b4863e514db0da
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-26 09:01:16 +0100

    Refactored pokeball-spinner.component

--- src/app/poke-ui/components/pokeball-spinner/pokeball-spinner.component.ts
+++ src/app/poke-ui/components/pokeball-spinner/pokeball-spinner.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pokeball-spinner',
   templateUrl: './pokeball-spinner.component.html',
   styleUrls: ['./pokeball-spinner.component.scss'],
 })
-export class PokeballSpinnerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class PokeballSpinnerComponent {}
