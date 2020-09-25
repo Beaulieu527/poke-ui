7236d5300fcd599dc0ffac8c4a074437cd16571d
Justin Beaulieu <justinbeaulieu@Justins-MBP.attlocal.net>
2020-09-25 16:37:33 -0500

    Wrote E2E testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,62 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Poke-UI', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message above search bar', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to poke-ui!');
+    expect(page.getTitleText()).toEqual('Search Your Pokedex');
+  });
+
+  it('should display a list of 10 pokemon', () => {
+    page.navigateTo();
+    expect(page.getPokemonList().count()).toBe(10);
+  });
+
+  it('should show a second page of list and show selected pokemon', () => {
+    page.navigateTo();
+    page.getNewPage(3);
+    page.getPokemonFromList(6);
+    expect(page.getPokemonName().getText()).toBe('#027Sandshrew');
+  });
+
+  it('should view selected pokemon', () => {
+    page.navigateTo();
+    page.getPokemonFromList(5);
+    expect(page.getPokemonName().getText()).toBe('#006Charizard');
+  });
+
+  it('should open and navigate between pokemon using arrow buttons', () => {
+    page.navigateTo();
+    page.getPokemonFromList(0);
+    page.clickLeftArrow();
+    expect(page.getPokemonName().getText()).toBe('#807Zeraora');
+    page.clickRightArrow();
+    page.clickRightArrow();
+    expect(page.getPokemonName().getText()).toBe('#002Ivysaur');
+  });
+
+  it('should search pokemon by ID and open page', () => {
+    page.navigateTo();
+    page.searchID(400);
+    expect(page.getPokemonName().getText()).toBe('#400Bibarel');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry),
+    );
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,42 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
+
+  getPokemonList() {
+    return element.all(by.css('.pokemon-list__element'));
+  }
+
+  getPokemonFromList(index) {
+    return element
+      .all(by.css('.pokemon-list__content'))
+      .get(index)
+      .click();
+  }
+
+  searchID(id) {
+    return (
+      element(by.css('#search')).click(),
+      element(by.css('#search')).sendKeys(id),
+      element(by.css('.form__button')).click()
+    );
+  }
+
+  getPokemonName() {
+    return element(by.css('#name'));
+  }
+
+  clickRightArrow() {
+    return element(by.css('.arrow__right')).click();
+  }
+
+  clickLeftArrow() {
+    return element(by.css('.arrow__left')).click();
+  }
+
+  getNewPage(index) {
+    return element
+      .all(by.css('.pagination__content'))
+      .get(index)
+      .click();
+  }
 }
